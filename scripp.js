66cb6de9fd22dbdd5db6244bb11085fb96fe8353
hoangson <tranhoangson03@gmail.com>
2024-11-16 08:32:35 +0700

    update 5

--- scripp.js
+++ scripp.js
@@ -1,35 +1,29 @@
 const tabItems = document.querySelectorAll('.tab-item');
 const tabContentItems = document.querySelectorAll('.tab-content-item');
-const tabBrg = document.querySelectorAll('.tab-brg');
 
 function selectItem(e) {
-
-	removeBorder();
-	removeShow();
-	this.classList.add('tab-border');
-	const tabContentItem = document.querySelector(`#${this.id}-content`);
-	tabContentItem.classList.add('show');
-}
-
-
-function removeBorder() {
-	tabItems.forEach(item => {
-		item.classList.remove('tab-brg');
-	});
+removeBorder();
+removeShow();
+this.classList.add('tab-border');
+this.querySelector('.button_seven').classList.add('tab-brg');
+const tabContentItem = document.querySelector(`#${this.id}-content`);
+tabContentItem.classList.add('show');
 }
 
 function removeBorder() {
-	tabItems.forEach(item => {
-		item.classList.remove('tab-border');
-	});
+tabItems.forEach(item => {
+    item.classList.remove('tab-border');
+    item.querySelector('.button_seven').classList.remove('tab-brg');
+});
 }
 
 function removeShow() {
-	tabContentItems.forEach(item => {
-		item.classList.remove('show');
-	});
+tabContentItems.forEach(item => {
+    item.classList.remove('show');
+});
 }
 
+
 tabItems.forEach(item => {
-	item.addEventListener('click', selectItem);
+item.addEventListener('click', selectItem);
 });
